cc650dec1869baa042701b46032809a3752a91df
Thanh Le <thanhlevan0203@gmail.com>
2022-08-23 23:10:13 -0700

    changed title display

--- myBlog/posts/models.py
+++ myBlog/posts/models.py
@@ -5,4 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=25)
     body = models.CharField(max_length=500)
     def __str__(self):
-        return f"(self.title)"
+        return self.title
+        
+
