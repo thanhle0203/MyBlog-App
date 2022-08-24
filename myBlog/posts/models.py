1b4e0a2852cc1448903154df9b2d431eee6f97a6
Thanh Le <thanhlevan0203@gmail.com>
2022-08-23 23:05:14 -0700

    Changing how data is displayed in Django Admin

--- myBlog/posts/models.py
+++ myBlog/posts/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=25)
     body = models.CharField(max_length=500)
-    
+    def __str__(self):
+        return f"(self.title)"
