721124eebd1dfe09860ba5dce84cd41e6089e592
Thanh Le <thanhlevan0203@gmail.com>
2022-08-24 00:18:33 -0700

    Added datatime in views

--- myBlog/website/views.py
+++ myBlog/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datatime
 
 # Create your views here.
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html", 
+                {"current_time": datetime.now()})
