1505d3cfa94922ffdb71bad34aa5caa6b325ad2e
Thanh Le <thanhlevan0203@gmail.com>
2022-08-25 17:15:16 -0700

    Saving form if data is valid

--- myBlog/myBlog/urls.py
+++ myBlog/myBlog/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from website.views import welcome
+from posts.views import new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", welcome)
+    path("", welcome),
+    path("new", new),
 ]
