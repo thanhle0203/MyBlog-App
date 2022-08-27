32e8480a8c5a344e14dfe65ad45808f27600bc38
Thanh Le <thanhlevan0203@gmail.com>
2022-08-27 15:38:32 -0700

    Correct post icone on nav bar

--- myBlog/posts/views.py
+++ myBlog/posts/views.py
@@ -1,7 +1,9 @@
+from email.mime import message
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
+from django.contrib import messages
 
 # Create your views here.
 def detail(request, id):
@@ -11,6 +13,7 @@ def detail(request, id):
 def delete(request, id):
     post = get_object_or_404(Post, pk=id)
     post.delete()
+    messages.warning(request, f'Posted Deleted')
     return redirect("/")
 
 @login_required()
@@ -19,6 +22,7 @@ def new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, f'Posted Created')
             return redirect("/")
     else:
         form = PostForm()
