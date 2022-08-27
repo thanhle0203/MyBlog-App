42ba5c7d1e8ebd1e8a2153c9a69d9e2f57db2cea
Thanh Le <thanhlevan0203@gmail.com>
2022-08-27 15:21:35 -0700

    Building the signup view template

--- myBlog/website/forms.py
+++ myBlog/website/forms.py
@@ -1,4 +1,3 @@
-from dataclasses import field
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -8,10 +7,9 @@ class UserRegisterForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password', 'password2']:
+        for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
-
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
-            
+        fields = ['username', 'email', 'password1', 'password2']
--- myBlog/website/views.py
+++ myBlog/website/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime
 from posts.models import Post
 from .forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def welcome(request):
-    return render(request, "website/welcome.html", 
-                {"current_time": datetime.now(),
-                "posts": Post.objects.all(),
-                "num_posts": Post.objects.count(),
-                })
-
+    return render(request, "website/welcome.html",
+                  {"current_time": datetime.now(),
+                   "posts": Post.objects.all(),
+                   "num_posts": Post.objects.count()})
+    
 def signup(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            new_user = authenticate(username=username, 
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            messages.success(request, f'Account created for {username}')
+            return redirect("/")
     else:
         form = UserRegisterForm()
-
     return render(request, "registration/signup.html", {"form": form})
 
