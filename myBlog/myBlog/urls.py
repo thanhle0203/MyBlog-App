7178e5a4c9e6601343218c95112a7890a5b31688
Thanh Le <thanhlevan0203@gmail.com>
2022-08-26 23:58:13 -0700

    Updated templates for get detail post and delete post

--- myBlog/myBlog/urls.py
+++ myBlog/myBlog/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", welcome, name='home'),
     path("posts/", include("posts.urls") ),
-    path("",)
+    
 ]
--- myBlog/posts/urls.py
+++ myBlog/posts/urls.py
@@ -10,6 +10,6 @@ from . import views
 urlpatterns = [
     path('new', views.new, name='new'),
     path('<int:id>', views.detail, name='detail'),
-    path('<delete/<int:id>', views.delete, name='delete'),
+    path('delete/<int:id>', views.delete, name='delete'),
 
 ]
